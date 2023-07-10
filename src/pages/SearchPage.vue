6e199c8e06367b52ae28b4dfbf25f838b98b5b4a
italocirillo <italocirillo@gmail.com>
2023-07-10 18:49:54 +0200

    SEARCH PAGE FIXED

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -52,39 +52,51 @@ export default {
 </script>
 
 <template>
-  <div v-if="this.loading === true">
-    <h1 class="text-center">...LOADING...</h1>
-  </div>
-  <div v-else>
-    <h2 class="text-center">Profiles:</h2>
-    <div class="container">
-      <div class="col">
-        <label class="form-label" for="technology">Technologies</label>
-        <select v-model="this.store.selectedTechnology" id="technology" class="form-select w-100" @change="getProfiles()">
-          <option selected value=""></option>
-          <option value="all">ALL</option>
-          <option v-for="technology_item in technologies" :key="technology_item.id" :value="technology_item.id">{{
-            technology_item.name }}</option>
-        </select>
-      </div>
-
-      <template v-if="this.profiles.length > 0">
-        <div class="row row-cols-3">
-          <div class="col g-4" v-for="profile in profiles">
-            <ProfileCard :profile="profile" />
-          </div>
-        </div>
-      </template>
-
-      <template v-else>
-        <p class="text-center text-uppercase py-3">
-          Technologies not found
-        </p>
-      </template>
+  <main>
+    <!-- Loading -->
+    <div v-if="this.loading === true">
+      <h1 class="text-center">...LOADING...</h1>
     </div>
-  </div>
+
+    <div v-else>
+      <div class="container">
+        <div class="col">
+          <label class="form-label" for="technology">Technologies</label>
+          <select v-model="this.store.selectedTechnology" id="technology" class="form-select w-100"
+            @change="getProfiles()">
+            <option selected value=""></option>
+            <option value="all">ALL</option>
+            <option v-for="technology_item in technologies" :key="technology_item.id" :value="technology_item.id">{{
+              technology_item.name }}</option>
+          </select>
+        </div>
+
+        <template v-if="this.profiles.length > 0">
+          <div class="row row-cols-3">
+            <div class="col g-4" v-for="profile in profiles">
+              <ProfileCard :profile="profile" />
+            </div>
+          </div>
+        </template>
+
+        <template v-else>
+          <p class="text-center text-uppercase py-3">
+            Technologies not found
+          </p>
+        </template>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style scoped lang="scss">
 @use "../styles/general.scss" as *;
+
+main {
+  padding-top: 4rem;
+  padding-bottom: 4rem;
+  background: #69788c;
+  background: -webkit-linear-gradient(58deg, #69788c 0%, #f0f3f4 100%);
+  background: linear-gradient(58deg, #69788c 0%, #f0f3f4 100%);
+}
 </style>
