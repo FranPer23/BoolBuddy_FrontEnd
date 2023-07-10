1e289cbee2b68e9c92c152fa3f5ca4293bf95606
italocirillo <italocirillo@gmail.com>
2023-07-10 17:31:06 +0200

    ADD LOADING

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -9,6 +9,7 @@ export default {
       profiles: [],
       store,
       technologies: [],
+      loading: false,
     };
   },
 
@@ -22,6 +23,8 @@ export default {
   },
   methods: {
     getProfiles(page = 1) {
+      this.loading = true;
+
       const params = {
         page: page,
       }
@@ -31,9 +34,12 @@ export default {
           params.technology_id = this.store.selectedTechnology;
         }
       }
-      axios.get(`${this.store.baseUrl}/api/profiles`, { params }).then((resp) => {
-        this.profiles = resp.data.results.data;
-      });
+      axios.get(`${this.store.baseUrl}/api/profiles`, { params })
+        .then((resp) => {
+          this.profiles = resp.data.results.data;
+        }).finally(() => {
+          this.loading = false;
+        });
     },
     getTechnologies() {
       axios.get(`${this.store.baseUrl}/api/technologies`).then((resp) => {
@@ -46,31 +52,36 @@ export default {
 </script>
 
 <template>
-  <h2 class="text-center">Profiles:</h2>
-  <div class="container">
-    <div class="col">
-      <label class="form-label" for="technology">Technologies</label>
-      <select v-model="this.store.selectedTechnology" id="technology" class="form-select w-100" @change="getProfiles()">
-        <option selected value=""></option>
-        <option value="all">ALL</option>
-        <option v-for="technology_item in technologies" :key="technology_item.id" :value="technology_item.id">{{
-          technology_item.name }}</option>
-      </select>
-    </div>
-
-    <template v-if="this.profiles.length > 0">
-      <div class="row row-cols-3">
-        <div class="col g-4" v-for="profile in profiles">
-          <ProfileCard :profile="profile" />
-        </div>
+  <div v-if="this.loading === true">
+    <h1 class="text-center">...LOADING...</h1>
+  </div>
+  <div v-else>
+    <h2 class="text-center">Profiles:</h2>
+    <div class="container">
+      <div class="col">
+        <label class="form-label" for="technology">Technologies</label>
+        <select v-model="this.store.selectedTechnology" id="technology" class="form-select w-100" @change="getProfiles()">
+          <option selected value=""></option>
+          <option value="all">ALL</option>
+          <option v-for="technology_item in technologies" :key="technology_item.id" :value="technology_item.id">{{
+            technology_item.name }}</option>
+        </select>
       </div>
-    </template>
 
-    <template v-else>
-      <p class="text-center text-uppercase py-3">
-        Technologies not found
-      </p>
-    </template>
+      <template v-if="this.profiles.length > 0">
+        <div class="row row-cols-3">
+          <div class="col g-4" v-for="profile in profiles">
+            <ProfileCard :profile="profile" />
+          </div>
+        </div>
+      </template>
+
+      <template v-else>
+        <p class="text-center text-uppercase py-3">
+          Technologies not found
+        </p>
+      </template>
+    </div>
   </div>
 </template>
 
