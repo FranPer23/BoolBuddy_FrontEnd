16182266b2cc418bb6de72ce81da5ece24928bfb
italocirillo <italocirillo@gmail.com>
2023-07-10 13:11:13 +0200

    store edit for windows

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-    // baseUrl: "http://localhost:8000"
-    baseUrl: "http://127.0.0.1:8000"
+    baseUrl: "http://localhost:8000"
+    //baseUrl: "http://127.0.0.1:8000"
 })
