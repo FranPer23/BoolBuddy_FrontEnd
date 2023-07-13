d519b5ff736f62a99766982e7cd19fec1208afaa
Cris-NTL <natolicristian92@gmail.com>
2023-07-13 10:24:37 +0200

    store.js

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-    baseUrl: "http://localhost:8000",
+    baseUrl: "http://localhost:3306",
     selectedTechnology: "all",
     //baseUrl: "http://127.0.0.1:8000"
 
