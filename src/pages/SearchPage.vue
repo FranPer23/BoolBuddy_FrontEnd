67fc2b52dd97281a34ded061db30c17d5c22f481
italocirillo <italocirillo@gmail.com>
2023-07-14 13:19:44 +0200

    filtro media voti implementato

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -9,6 +9,7 @@ export default {
       profiles: [],
       store,
       loading: false,
+      averageVote: "all",
     };
   },
 
@@ -22,6 +23,7 @@ export default {
   },
   methods: {
     getProfiles(page = 1) {
+      console.log(this.averageVote);
       this.loading = true;
 
       const params = {
@@ -33,6 +35,13 @@ export default {
           params.technology_id = this.store.selectedTechnology;
         }
       }
+
+      if (this.averageVote !== "") {
+        if (this.averageVote !== "all") {
+          params.averageVote = this.averageVote;
+        }
+      }
+
       axios
         .get(`${this.store.baseUrl}/api/profiles`, { params })
         .then((resp) => {
@@ -57,18 +66,18 @@ export default {
     <div class="bg_div">
       <!-- Loading -->
       <div v-if="this.loading === true" class="load_container">
-      <div class="ms_loader">
-        <div class="spinner-border" role="status">
-          <span class="visually-hidden">Loading...</span>
+        <div class="ms_loader">
+          <div class="spinner-border" role="status">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+        </div>
+        <div class="loading_text">
+          <h2>...LOADING...</h2>
         </div>
       </div>
-      <div class="loading_text">
-        <h2>...LOADING...</h2>
-      </div>
-    </div>
-  
+
       <div v-else class="container">
-        
+
         <div class="col-11 d-flex justify-content-center">
           <label class="form-label ms-5" for="technology">Technologies</label>
           <select v-model="this.store.selectedTechnology" id="technology" class="form-select ms-3"
@@ -79,8 +88,21 @@ export default {
               technology_item.name }}</option>
           </select>
         </div>
-        <div class="">
-  
+
+        <div class="col-11 d-flex justify-content-center">
+          <label class="form-label ms-5 my-3" for="averageVote">Average Vote</label>
+          <select v-model="this.averageVote" id="averageVote" class="form-select ms-3 w-75 my-3" @change="getProfiles()">
+            <option selected value=""></option>
+            <option value="all">ALL</option>
+            <option value="1">>1</option>
+            <option value="2">>2</option>
+            <option value="3">>3</option>
+            <option value="4">>4</option>
+            <option value="5">>5</option>
+          </select>
+        </div>
+
+        <div>
           <div v-if="this.profiles.length > 0">
             <div class="row row-cols-3 ms-1">
               <div class="col g-4" v-for="profile in profiles">
@@ -88,7 +110,7 @@ export default {
               </div>
             </div>
           </div>
-  
+
           <div v-else>
             <p class="text-center text-uppercase py-3">
               No match for your search
@@ -103,11 +125,13 @@ export default {
 
 <style scoped lang="scss">
 @use "../styles/general.scss" as *;
+
 label {
   font-size: 1.5rem;
   color: rgba(255, 255, 255, 0.604);
 }
-.bg_div{
+
+.bg_div {
   background-color: rgba(0, 0, 0, 0.596);
   padding-top: 10rem;
   padding-bottom: 2rem;
@@ -116,13 +140,14 @@ label {
 main {
   background-image: url(../assets/defaultimg/sfondo4.jpg);
   background-size: cover;
-  background-position: center; 
+  background-position: center;
 }
 
 //LOADING
 .load_container {
   position: relative;
 }
+
 .ms_loader {
   position: absolute;
   left: 50%;
