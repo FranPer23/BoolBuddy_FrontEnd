9f47dac028e4dfd78e0d0238a6ef773860ba1fb2
Giadagenovesi <giada.g@outlook.it>
2023-07-14 19:07:30 +0200

    Add Edits

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -1,26 +1,37 @@
 <script>
-import { store } from "../store";
+    import { store } from "../store";
 
-export default {
-  name: "ProfileCard",
-  props: {
-    profile: Object,
-  },
-  data() {
-    return {
-      store,
+    export default {
+        name: "ProfileCard",
+        props: {
+            profile: Object,
+        },
+        data() {
+            return {
+                store,
+            };
+        },
+        computed: {
+            imageUrl() {
+                return `${this.store.baseUrl}/storage${this.profile.photo}`;
+            },
+        },
+        methods: {
+            avg(voti) {
+                console.log(voti);
+                let sum = 0;
+                voti.forEach(voto => {
+                    sum = parseInt(voto.vote) + sum;
+                });
+                // console.log(sum / voti.length);
+                return (sum / voti.length);
+            }
+        }
     };
-  },
-  computed: {
-    imageUrl() {
-      return `${this.store.baseUrl}/storage${this.profile.photo}`;
-    },
-  },
-};
 </script>
 
 <template>
-  <!-- <div class="ms_card mb-4">
+    <!-- <div class="ms_card mb-4">
     <div class="card-inner">
       <div class="card-front">
         <div v-if="profile.photo">
@@ -49,127 +60,135 @@ export default {
       </div>
     </div>
   </div> -->
-  
-  <!-- prova -->
-  <div class="card-client">
-    <div class="user-picture">
-      <div v-if="profile.photo">
-        <img :src="imageUrl" alt="" />
-      </div>
-      <div v-else>
-        <img src="../assets/defaultimg/profilejpg.jpg" alt="" />
-      </div>
-    </div>
-    <p class="name-client text-center"> {{ profile.surname }} {{ profile.name }}
-    </p>
-    <hr>
-    <div class="ms_technology_space">
-      <ul class="list-unstyled flex-wrap d-flex justify-content-between">
-        <li v-for="technology in profile.technology" class="col-6">
-          {{ technology.name }}
-        </li>
-      </ul>
-    </div>
-    <div class="d-flex justify-content-center">
-      <router-link :to="{ name: 'single-profile', params: { id: profile.id } }"
-        class="ms_btn text-center align-self-center">
-        Details
-      </router-link>
 
+    <!-- prova -->
+    <div class="card-client">
+        <div class="user-picture">
+            <div v-if="profile.photo">
+                <img :src="imageUrl" alt="" />
+            </div>
+            <div v-else>
+                <img src="../assets/defaultimg/profilejpg.jpg" alt="" />
+            </div>
+        </div>
+        <p class="name-client text-center"> {{ profile.surname }} {{ profile.name }}
+        </p>
+        <hr>
+        <div class="ms_technology_space">
+            <ul class="list-unstyled flex-wrap d-flex justify-content-between">
+                <li v-for="technology in profile.technology" class="col-6">
+                    {{ technology.name }}
+                </li>
+            </ul>
+        </div>
+
+        <div class="d-flex justify-content-center mb-3">
+            <div v-for="n in 5">
+                <i v-if="avg(profile.votes) >= n" class="fa-solid fa-star me-3 ms_vote"></i>
+                <i v-else class="fa-regular fa-star me-3 ms_vote"></i>
+            </div>
+        </div>
+
+        <div class="d-flex justify-content-center">
+            <router-link :to="{ name: 'single-profile', params: { id: profile.id } }"
+                class="ms_btn text-center align-self-center">
+                Details
+            </router-link>
+
+        </div>
     </div>
-  </div>
 </template>
 
 <style lang="scss" scoped>
-// STYLE
-img {
-  width: 10rem;
-  height: 10rem;
-  border-radius: 50%;
-}
+    // STYLE
+    img {
+        width: 10rem;
+        height: 10rem;
+        border-radius: 50%;
+    }
 
-h2 {
-  font-size: 2.5rem;
-}
+    h2 {
+        font-size: 2.5rem;
+    }
 
-// CARD
-.ms_technology_space {
-  font-size: 1rem;
-  height: 100px;
-}
-.card-client {
-  background-color: rgba(255, 255, 255, 0.692);
-  padding-top: 25px;
-  padding-bottom: 25px;
-  padding-left: 20px;
-  padding-right: 20px;
-  border: 4px solid rgb(190, 187, 187);
-  box-shadow: 0 6px 10px rgb(18, 18, 19);
-  border-radius: 10px;
+    // CARD
+    .ms_technology_space {
+        font-size: 1rem;
+        height: 100px;
+    }
 
-  color: #0c2230;
-  font-family: "Poppins", sans-serif;
-  transition: all 0.3s ease;
-}
+    .card-client {
+        background-color: rgba(255, 255, 255, 0.692);
+        padding-top: 25px;
+        padding-bottom: 25px;
+        padding-left: 20px;
+        padding-right: 20px;
+        border: 4px solid rgb(190, 187, 187);
+        box-shadow: 0 6px 10px rgb(18, 18, 19);
+        border-radius: 10px;
 
-.card-client:hover {
-  transform: translateY(-10px);
-}
+        color: #0c2230;
+        font-family: "Poppins", sans-serif;
+        transition: all 0.3s ease;
+    }
 
-.user-picture {
-  overflow: hidden;
-  object-fit: cover;
-  width: 10rem;
-  height: 10rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: auto;
-}
+    .card-client:hover {
+        transform: translateY(-10px);
+    }
+
+    .user-picture {
+        overflow: hidden;
+        object-fit: cover;
+        width: 10rem;
+        height: 10rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: auto;
+    }
 
 
-.name-client {
-  margin: 0;
-  margin-top: 20px;
-  font-weight: 600;
-  font-size: 18px;
-}
+    .name-client {
+        margin: 0;
+        margin-top: 20px;
+        font-weight: 600;
+        font-size: 18px;
+    }
 
-// button style
+    // button style
 
-.ms_btn {
-  border-radius: 10px;
-  width: 50%;
-  display: inline-block;
-  padding: 0.5rem 0.5rem;
-  font-size: 16px;
-  font-weight: 700;
-  color: #0c2230;
-  border: 3px solid #0c2230;
-  cursor: pointer;
-  position: relative;
-  background-color: transparent;
-  text-decoration: none;
-  overflow: hidden;
-  z-index: 1;
-  font-family: inherit;
-}
+    .ms_btn {
+        border-radius: 10px;
+        width: 50%;
+        display: inline-block;
+        padding: 0.5rem 0.5rem;
+        font-size: 16px;
+        font-weight: 700;
+        color: #0c2230;
+        border: 3px solid #0c2230;
+        cursor: pointer;
+        position: relative;
+        background-color: transparent;
+        text-decoration: none;
+        overflow: hidden;
+        z-index: 1;
+        font-family: inherit;
+    }
 
-.ms_btn::before {
-  content: "";
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  background-color: #f0f3f4;
-  transform: translateX(-100%);
-  transition: all 0.3s;
-  z-index: -1;
-}
+    .ms_btn::before {
+        content: "";
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        background-color: #f0f3f4;
+        transform: translateX(-100%);
+        transition: all 0.3s;
+        z-index: -1;
+    }
 
-.ms_btn:hover::before {
-  transform: translateX(0);
-}
+    .ms_btn:hover::before {
+        transform: translateX(0);
+    }
 </style>
-
--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -1,168 +1,171 @@
 <script>
-import axios from "axios";
-import ProfileCard from "../components/ProfileCard.vue";
-import { store } from "../store";
+    import axios from "axios";
+    import ProfileCard from "../components/ProfileCard.vue";
+    import { store } from "../store";
 
-export default {
-  data() {
-    return {
-      profiles: [],
-      store,
-      loading: false,
-      averageVote: "all",
+    export default {
+        data() {
+            return {
+                profiles: [],
+                store,
+                loading: false,
+                averageVote: "all",
+            };
+        },
+
+        name: "SearchPage",
+        components: {
+            ProfileCard,
+        },
+        mounted() {
+            this.getTechnologies();
+            this.getProfiles();
+        },
+        methods: {
+            getProfiles(page = 1) {
+                console.log(this.averageVote);
+                this.loading = true;
+
+                const params = {
+                    page: page,
+                };
+
+                if (this.store.selectedTechnology !== "") {
+                    if (this.store.selectedTechnology !== "all") {
+                        params.technology_id = this.store.selectedTechnology;
+                    }
+                }
+
+                if (this.averageVote !== "") {
+                    if (this.averageVote !== "all") {
+                        params.averageVote = this.averageVote;
+                    }
+                }
+
+                axios
+                    .get(`${this.store.baseUrl}/api/profiles`, { params })
+                    .then((resp) => {
+                        this.profiles = resp.data.results.data;
+                    })
+                    .finally(() => {
+                        this.loading = false;
+                    });
+            },
+            getTechnologies() {
+                axios.get(`${this.store.baseUrl}/api/technologies`).then((resp) => {
+                    this.technologies = resp.data.results;
+                    console.log(resp.data.results);
+                });
+            },
+        },
     };
-  },
-
-  name: "SearchPage",
-  components: {
-    ProfileCard,
-  },
-  mounted() {
-    this.getTechnologies();
-    this.getProfiles();
-  },
-  methods: {
-    getProfiles(page = 1) {
-      console.log(this.averageVote);
-      this.loading = true;
-
-      const params = {
-        page: page,
-      };
-
-      if (this.store.selectedTechnology !== "") {
-        if (this.store.selectedTechnology !== "all") {
-          params.technology_id = this.store.selectedTechnology;
-        }
-      }
-
-      if (this.averageVote !== "") {
-        if (this.averageVote !== "all") {
-          params.averageVote = this.averageVote;
-        }
-      }
-
-      axios
-        .get(`${this.store.baseUrl}/api/profiles`, { params })
-        .then((resp) => {
-          this.profiles = resp.data.results.data;
-        })
-        .finally(() => {
-          this.loading = false;
-        });
-    },
-    getTechnologies() {
-      axios.get(`${this.store.baseUrl}/api/technologies`).then((resp) => {
-        this.technologies = resp.data.results;
-        console.log(resp.data.results);
-      });
-    },
-  },
-};
 </script>
 
 <template>
-  <main>
-    <div class="bg_div">
-      <!-- Loading -->
-      <div v-if="this.loading === true" class="load_container">
-        <div class="ms_loader">
-          <div class="spinner-border" role="status">
-            <span class="visually-hidden">Loading...</span>
-          </div>
-        </div>
-        <div class="loading_text">
-          <h2>...LOADING...</h2>
-        </div>
-      </div>
-
-      <div v-else class="container">
-
-        <div class="col-11 d-flex justify-content-center">
-          <label class="form-label ms-5" for="technology">Technologies</label>
-          <select v-model="this.store.selectedTechnology" id="technology" class="form-select ms-3"
-            @change="getProfiles()">
-            <option selected value=""></option>
-            <option value="all">ALL</option>
-            <option v-for="technology_item in technologies" :key="technology_item.id" :value="technology_item.id">{{
-              technology_item.name }}</option>
-          </select>
-        </div>
-
-        <div class="col-11 d-flex justify-content-center">
-          <label class="form-label ms-5 my-3" for="averageVote">Average Vote</label>
-          <select v-model="this.averageVote" id="averageVote" class="form-select ms-3 w-75 my-3" @change="getProfiles()">
-            <option selected value=""></option>
-            <option value="all">ALL</option>
-            <option value="1">>1</option>
-            <option value="2">>2</option>
-            <option value="3">>3</option>
-            <option value="4">>4</option>
-            <option value="5">>5</option>
-          </select>
-        </div>
-
-        <div>
-          <div v-if="this.profiles.length > 0">
-            <div class="row row-cols-3 ms-1">
-              <div class="col g-4" v-for="profile in profiles">
-                <ProfileCard :profile="profile" />
-              </div>
+    <main>
+        <div class="bg_div">
+            <!-- Loading -->
+            <div v-if="this.loading === true" class="load_container">
+                <div class="ms_loader">
+                    <div class="spinner-border" role="status">
+                        <span class="visually-hidden">Loading...</span>
+                    </div>
+                </div>
+                <div class="loading_text">
+                    <h2>...LOADING...</h2>
+                </div>
             </div>
-          </div>
 
-          <div v-else>
-            <p class="text-center text-uppercase py-3">
-              No match for your search
-            </p>
-          </div>
+            <div v-else class="container">
+
+                <div class="col-11 d-flex justify-content-center">
+                    <label class="form-label ms-5" for="technology">Technologies</label>
+                    <select v-model="this.store.selectedTechnology" id="technology" class="form-select ms-3"
+                        @change="getProfiles()">
+                        <option selected value=""></option>
+                        <option value="all">ALL</option>
+                        <option v-for="technology_item in technologies" :key="technology_item.id"
+                            :value="technology_item.id">{{
+                            technology_item.name }}</option>
+                    </select>
+                </div>
+
+                <div class="col-11 d-flex justify-content-center">
+                    <label class="form-label ms-5 my-3" for="averageVote">Average Vote</label>
+                    <select v-model="this.averageVote" id="averageVote" class="form-select ms-3 w-75 my-3"
+                        @change="getProfiles()">
+                        <option selected value=""></option>
+                        <option value="all">ALL</option>
+                        <option value="1">>= 1</option>
+                        <option value="2">>= 2</option>
+                        <option value="3">>= 3</option>
+                        <option value="4">>= 4</option>
+                        <option value="5">>= 5</option>
+                    </select>
+                </div>
+
+                <div>
+                    <div v-if="this.profiles.length > 0">
+                        <div class="row row-cols-3 ms-1">
+                            <div class="col g-4" v-for="profile in profiles">
+                                <ProfileCard :profile="profile" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div v-else>
+                        <p class="text-center text-uppercase py-3">
+                            No match for your search
+                        </p>
+                    </div>
+                </div>
+            </div>
+
         </div>
-      </div>
-
-    </div>
-  </main>
+    </main>
 </template>
 
 <style scoped lang="scss">
-@use "../styles/general.scss" as *;
+    @use "../styles/general.scss" as *;
 
-label {
-  font-size: 1.5rem;
-  color: rgba(255, 255, 255, 0.604);
-}
+    label {
+        font-size: 1.5rem;
+        color: rgba(255, 255, 255, 0.604);
+    }
 
-.bg_div {
-  background-color: rgba(0, 0, 0, 0.596);
-  padding-top: 10rem;
-  padding-bottom: 2rem;
-}
+    .bg_div {
+        background-color: rgba(0, 0, 0, 0.596);
+        padding-top: 10rem;
+        padding-bottom: 2rem;
+    }
 
-main {
-  background-image: url(../assets/defaultimg/sfondo4.jpg);
-  background-size: cover;
-  background-position: center;
-}
+    main {
+        background-image: url(../assets/defaultimg/sfondo4.jpg);
+        background-size: cover;
+        background-position: center;
+    }
 
-//LOADING
-.load_container {
-  position: relative;
-}
+    //LOADING
+    .load_container {
+        position: relative;
+    }
 
-.ms_loader {
-  position: absolute;
-  left: 50%;
-  top: 40%;
-  z-index: 1000;
-  height: 31px;
-  width: 31px;
-  color: white;
-}
+    .ms_loader {
+        position: absolute;
+        left: 50%;
+        top: 40%;
+        z-index: 1000;
+        height: 31px;
+        width: 31px;
+        color: white;
+    }
 
-.loading_text {
-  padding-top: 5rem;
-  display: flex;
-  justify-content: center;
-  color: white;
-}
+    .loading_text {
+        padding-top: 5rem;
+        display: flex;
+        justify-content: center;
+        color: white;
+    }
 </style>
-
+searchPage.html
+6 KB
